961dff524239746db7d6fd28e98b54c56ec9859d
Yumeme <yume@sfc.wide.ad.jp>
2018-09-04 21:44:48 +0900

    hello.java

---
+++ hello.java
@@ -0,0 +1,17 @@
+//0xomochi
+//hello.java
+
+//ファイル名とクラス名は一致させる必要あり
+//大文字と小文字は区別される
+
+//class ファイル名(クラス名){
+//    public static void main(String[] args){
+//    (処理内容)
+//    }
+//}
+
+class hello{
+    public static void main(String args[]){
+        System.out.println("Hello, world!");
+    }
+}
